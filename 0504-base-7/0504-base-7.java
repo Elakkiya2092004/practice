c61ec141c8095dff4ac677a995c0d9da546f7ec9
Elakkiya2092004 <elakkiyaswathy1@gmail.com>
2025-10-12 15:12:10 +0530

    Time: 1 ms (75.04%), Space: 41.2 MB (58.27%) - LeetHub

---
+++ 0504-base-7/0504-base-7.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String convertToBase7(int num) {
+         if(num == 0) {
+            return "0";
+            }
+        boolean isNegative = num < 0;
+        num = Math.abs(num);
+        StringBuilder sb = new StringBuilder();
+
+        // remainder = dividend % divisor
+        while( num > 0){
+        int reminder = 0;
+            reminder = num % 7;  
+            sb.append(reminder);
+            num /= 7; 
+        }
+
+        if(isNegative) sb.append("-");
+        return sb.reverse().toString();
+    }
+}
