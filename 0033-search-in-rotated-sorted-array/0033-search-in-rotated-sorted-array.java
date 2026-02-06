58b2189607070860756ff228f57298299732a74e
Elakkiya2092004 <elakkiyaswathy1@gmail.com>
2026-02-06 14:46:56 +0530

    Time: 0 ms (100%), Space: 43.8 MB (79.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,11 +1,10 @@
 class Solution {
     public int search(int[] arr, int target) {
-        int res=-1;
+       int res=-1;
        for(int i=0;i<arr.length;i++){
         if(arr[i]==target){ 
         res=i;}
         }
-    return res;
+    return res; 
     }
-    
 }
